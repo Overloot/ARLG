5aca2bdadb7affc37f78daaa6d23f90b2b43571c
Apromix <bees@meta.ua>
2015-11-19 23:40:43 +0200

    Исправление в ачивках.

--- src/com/rommax/Achievement.java
+++ src/com/rommax/Achievement.java
@@ -6,6 +6,7 @@ public class Achievement{
 
 	public static int[] counter;
 	public static int[] unlock;
+	private static Game game;
 	
 	static {
 		counter = new int[AchievementSet.MAX_ACHIEVEMENTS];
@@ -21,37 +22,41 @@ public class Achievement{
 		return unlock[i] == 0 || unlock[i] == 2;
 	}
 	
+	// Ачивка разблокирована
+	private static void done(Game game, int i){
+		if (i > 0 && AchievementSet.getAchievement(i - 1).getType() == AchievementSet.getAchievement(i).getType())
+			unlock[i - 1] = 2;
+		unlock[i] = 1;
+		game.logMessage("#5#Новое достижение: " + 
+			AchievementSet.getAchievement(i).getName().toUpperCase() + "#^#/#");
+	}
+	
 	// Проверка
 	public static void check(Game game, int type, int value){
 		
-		boolean flag = false;
 		for(int i = 0; i <= AchievementSet.MAX_ACHIEVEMENTS - 1; i++){
 
 			if (unlock[i] > 0) continue;
-			flag = false;
 		
 			if (AchievementSet.getAchievement(i).getType() == type){
 				switch(type){
+					// По значению
 					case AchievementSet.TYPE_FIND_LEVEL:
 						if(AchievementSet.getAchievement(i).getValue() == value){
-							flag = true;
-							unlock[i] = 1;
+							done(game, i);
+							continue;
 						}
 						break;
+					// По счетчику
 					case AchievementSet.TYPE_KILL_ENEMY:
 					case AchievementSet.TYPE_FIND_ITEM:
 						counter[i] = counter[i] + value;
 						if(AchievementSet.getAchievement(i).getValue() <= counter[i]){
-							if (i > 0 && AchievementSet.getAchievement(i - 1).getType() == AchievementSet.getAchievement(i).getType())
-								unlock[i - 1] = 2;
-							flag = true;
-							unlock[i] = 1;
+							done(game, i);
+							continue;
 						}
 						break;
 				}
-				if (flag)
-					game.logMessage("#5#Новое достижение: " + 
-						AchievementSet.getAchievement(i).getName().toUpperCase() + "#^#/#");
 			}
 		}
 	}
--- src/com/rommax/AchievementSet.java
+++ src/com/rommax/AchievementSet.java
@@ -27,13 +27,13 @@ public class AchievementSet {
 	static{
 		ACHIEVEMENTSET = new BaseAchievement[MAX_ACHIEVEMENTS];
 		
-		add("В глубину I!", "Достигнут 2-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 1, 1);
-		add("В глубину II!", "Достигнут 3-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 2, 2);
-		add("В глубину III!", "Достигнут 4-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 3, 3);
-
 		add("Охотник I", "Победил 3 врагов", "res/achievements/achievement2.png", TYPE_KILL_ENEMY, 3, 1);
 		add("Охотник II", "Победил 4 врагов", "res/achievements/achievement2.png", TYPE_KILL_ENEMY, 4, 2);
 		add("Охотник III", "Победил 5 врагов", "res/achievements/achievement2.png", TYPE_KILL_ENEMY, 5, 3);
+
+		add("В глубину I!", "Достигнут 2-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 1, 1);
+		add("В глубину II!", "Достигнут 3-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 2, 2);
+		add("В глубину III!", "Достигнут 4-й уровень подземелья", "res/achievements/achievement1.png", TYPE_FIND_LEVEL, 3, 3);
 
 		add("Собиратель I", "Нашел 3 предмета", "res/achievements/achievement3.png", TYPE_FIND_ITEM, 3, 1);
 		add("Собиратель II", "Нашел 4 предмета", "res/achievements/achievement3.png", TYPE_FIND_ITEM, 4, 2);
