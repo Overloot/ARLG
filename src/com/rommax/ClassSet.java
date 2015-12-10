49c271d562cc00815a8a2d1c316100905fa01d8b
Apromix <bees@meta.ua>
2015-12-10 04:18:17 +0200

    Добавлено описание класса воин.

--- src/com/rommax/ClassSet.java
+++ src/com/rommax/ClassSet.java
@@ -13,7 +13,7 @@ public class ClassSet {
     static{
 		
 		CLASSSET = new BaseClass[MAX_CLASSES];
-        CLASSSET[CLASS_FIGHTER] = new BaseClass(CLASS_FIGHTER, "Боец", "fighter", "1", "2", "3", 2, 0, 2, 0, SkillSet.SKILL_FIGHTER_1, SkillSet.SKILL_FIGHTER_2, SkillSet.SKILL_FIGHTER_3);
+        CLASSSET[CLASS_FIGHTER] = new BaseClass(CLASS_FIGHTER, "Боец", "fighter", "Это воин посвятивший себя множеству изнуряющих тренировок и прошедший", "через множество поединков, став лучшим из лучших. Может быстро перемещаться по", "полю боя и наносить точные и сильные удары по противнику.", 2, 0, 2, 0, SkillSet.SKILL_FIGHTER_1, SkillSet.SKILL_FIGHTER_2, SkillSet.SKILL_FIGHTER_3);
         CLASSSET[CLASS_RANGER] = new BaseClass(CLASS_RANGER, "Рейнжер", "ranger", "1", "2", "3", 1, 3, 0, 0, SkillSet.SKILL_RANGER_1, SkillSet.SKILL_RANGER_2, SkillSet.SKILL_RANGER_3);
         CLASSSET[CLASS_THIEF] = new BaseClass(CLASS_THIEF, "Вор", "thief", "Вор никогда не вступает в честный поединок, говорят что хорошего вора вообще", "сложно увидеть в бою, так как сам он в драки не ввязывается, а если заметит бой", "то подождет его окончания, а потом прикончит выживших и заберет все вещи.", 0, 3, -1, 6, SkillSet.SKILL_THIEF_1, SkillSet.SKILL_THIEF_2, SkillSet.SKILL_THIEF_3);
 		
